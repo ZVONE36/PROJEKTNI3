9c2d475104574639914532ebbd045c9df805b74d
ZVONE36 <zskvorc@tvz.hr>
2023-04-16 17:40:32 +0200

    Projektni 3

--- kategorija/unos_kategorije.py
+++ kategorija/unos_kategorije.py
@@ -2,11 +2,12 @@ from artikl import unos_artikla
 def unos_kategorije(redni_broj):
     kategorija = {}
     kategorija['naziv'] = input(f'Unesite naziv {redni_broj}. kategorije: ')
-    kategorija['artikli'] = []
+    artikli = []
 
     br_artikala = int(input(f'Unesite broj artikala za {redni_broj}. kategoriju: '))
     for i in range(1, br_artikala + 1):
         artikl = unos_artikla(i)
-        kategorija['artikli'].append(artikl)
+        artikli.append(artikl)
 
+    kategorija['artikli'] = artikli
     return kategorija
